01bde8592e627c878b40e827ec657279e79fc4bf
yogi43 <yogendrapandey43@gmail.com>
2017-01-18 00:52:34 +0530

    updated dbURI connection

--- employee-directory-mean-app/models/db.js
+++ employee-directory-mean-app/models/db.js
@@ -1,17 +1,15 @@
 var mongoose = require( 'mongoose' );
 
-var dbURI = 'mongodb://localhost/employeedb';
+//var dbURI = 'mongodb://localhost/employeedb';
 
+var dbURI = 'mongodb://yogi:pandey-1992@ds111489.mlab.com:11489/employees';
 
-//var dbURI = 'mongodb://<dbuser>:<dbpassword>@ds131878.mlab.com:31878/employeedb';
-
-
-mongoose.connect(dbURI);
-
+//mongoose.connect(dbURI);
 
 mongoose.connect(dbURI, function(err) {
 	if (err) {
 		console.log('connection error', err);
+    console.log('dbURI is: '+dbURI);
 	} else {
 		console.log('connection successful');
 	}
@@ -24,6 +22,6 @@ var employeeSchema = new mongoose.Schema({
 	Date_of_birth: Date,
 	Department: String,
 	Gender: String,
-	Age: ({ $subtract: [ new Date(), "$Date_of_birth" ] })/(365*24*60*60*1000) 
+	Age: ({ $subtract: [ new Date(), "$Date_of_birth" ] })/(365*24*60*60*1000)
 });
 mongoose.model("Employee",employeeSchema);
